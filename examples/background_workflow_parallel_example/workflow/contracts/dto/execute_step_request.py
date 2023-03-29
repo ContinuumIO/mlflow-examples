302df97c848ebed7e96641b907e6dada6c490cd5
Josh Burt <jburt@anaconda.com>
2023-03-29 12:57:31 -0700

    remove comment block

--- examples/background_workflow_parallel_example/workflow/contracts/dto/execute_step_request.py
+++ examples/background_workflow_parallel_example/workflow/contracts/dto/execute_step_request.py
@@ -5,21 +5,22 @@ from anaconda.enterprise.server.contracts import BaseModel
 
 class ExecuteStepRequest(BaseModel):
     """
-#     entry_point: str
-#         The workflow step to execute.
-#     parameters: Dict
-#         The dictionary of parameters to pass to the workflow step.
-#     run_id: Optional[str] = None
-#         If provided it is supplied and used for reporting.
-#     backend: str = "local"
-#         Default to `local` unless another is provided.
-#     synchronous: bool = True
-#         Controls whether to return immediately or after run completion.
-#     run_name: Optional[str] = None
-#         If provided it is supplied and used for reporting.
-#     resource_profile: str
-#         The resource profile to run the step on (if using the adsp backend)
+    entry_point: str
+        The workflow step to execute.
+    parameters: Dict
+        The dictionary of parameters to pass to the workflow step.
+    run_id: Optional[str] = None
+        If provided it is supplied and used for reporting.
+    backend: str = "local"
+        Default to `local` unless another is provided.
+    synchronous: bool = True
+        Controls whether to return immediately or after run completion.
+    run_name: Optional[str] = None
+        If provided it is supplied and used for reporting.
+    resource_profile: str
+        The resource profile to run the step on (if using the adsp backend)
     """
+
     uri: str = "."
     entry_point: str = "main"
     parameters: Optional[Dict] = None
